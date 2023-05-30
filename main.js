4ff11eb54cd3c896b7d532edd00595498ce0c97b
José Miguel <malikjmiguel0@gmail.com>
2023-05-30 20:58:52 -0300

    adiciona remover

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-import {adicionar, getItens} from "./store.js";     //importa store.js
+import {adicionar, getItens, remover} from "./store.js";     //importa store.js
 
 
 
     const form = document.forms.entrada;    //cria variável pro formulario
     form.addEventListener('submit', envia); //quando submit for acionado, chama a funcao envia
+    form.remover.addEventListener('click', remove);
 
     atualiza(); 
+    
+    function remove() {
+        console.log("Remove clickado!");
+        remover();
+        atualiza();
+    }
 
     function envia(evento) {
         evento.preventDefault();    //Não deixa enviar os dados para o backEnd, mantém no front
@@ -15,6 +22,7 @@ import {adicionar, getItens} from "./store.js";     //importa store.js
         adicionar(n);   //diz ele q todo array é pilha
         form.valor.value = "";
         form.valor.focus();     //voltar cursor
+        
 
         atualiza();
         }
@@ -24,14 +32,16 @@ import {adicionar, getItens} from "./store.js";     //importa store.js
         ol.innerHTML = "";
         const itens = getItens();
 
-        for(let i = 0; i < itens.length; i++){      //
+        for(let i = 0; i < itens.length; i++){      //um for para o tamanho do vetor de getItens()
             const li = document.createElement('li');    //cria 'li'
             li.textContent = itens[i];
             ol.appendChild(li);
 
         }
+
         itens.push('Boom!');
     
     }
+
 
         
