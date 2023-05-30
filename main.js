45b18217ef2cf53f032d7d74de7c9031a2242f4d
José Miguel <malikjmiguel0@gmail.com>
2023-05-30 19:56:29 -0300

    adiciona exportação padrão

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { estado } from "./store.js";
+import store from "./store.js";
 
     const form = document.forms.entrada;    //cria variável pro formulario
     form.addEventListener('submit', envia); //quando submit for acionado, chama a funcao envia
@@ -8,13 +8,13 @@ import { estado } from "./store.js";
     function envia(evento) {
         evento.preventDefault();    //Não deixa enviar os dados para o backEnd, mantém no front
         console.log('Formulário enviado!');
-        estado++;
+        store.estado++;
         atualiza();
         }
 
     function atualiza() {
         const ol = document.querySelector('ol');
-        ol.innerHTML = `<li>${estado}</li>`;
+        ol.innerHTML = `<li>${store.estado}</li>`;
     
     }
 
--- store.js
+++ store.js
@@ -1,1 +1,13 @@
-export let estado = 0;
+export const store = {
+    estado: 0
+};
+
+export default store;
+
+export const store2 = {
+    estado: 0
+};
+
+export const store3 = {
+    estado: 0
+};
