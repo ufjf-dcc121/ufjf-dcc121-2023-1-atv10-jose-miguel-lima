6209dd7ceb52b3d99aa8c6ae32377ece013e9357
José Miguel <malikjmiguel0@gmail.com>
2023-05-30 19:28:20 -0300

    adiciona estado basico

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+    const form = document.forms.entrada;    //cria variável pro formulario
+    form.addEventListener('submit', envia); //quando submit for acionado, chama a funcao envia
 
-        const form = document.forms.entrada;    //cria variável pro formulario
-        form.addEventListener('submit', envia); //quando submit for acionado, chama a funcao envia
+    let estado = 0;
 
-        function envia(evento) {
-            evento.preventDefault();    //Não deixa enviar os dados para o backEnd, mantém no front
-            console.log('Formulário enviado!');
+    function envia(evento) {
+        evento.preventDefault();    //Não deixa enviar os dados para o backEnd, mantém no front
+        console.log('Formulário enviado!');
+        estado++;
+        atualiza();
         }
+
+    function atualiza() {
+        const ol = document.querySelector('ol');
+        ol.innerHTML = `<li>${estado}</li>`;
+    
+    }
 
         
